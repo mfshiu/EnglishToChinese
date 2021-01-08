54a229214478a3150228de852eaf33c89d92be89
mfshiu <avatar.xu@gmail.com>
2021-01-08 20:11:26 +0800

    Add CKIP

--- convert.py
+++ convert.py
@@ -4,10 +4,10 @@ import os
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-ws = WS("./ckipdata")  # , disable_cuda=not GPU)
+ws = WS("./ckipdata", disable_cuda=False)  # , disable_cuda=not GPU)
 sentences = [
     "但美国并没有因此而遭遇超越国界的系统性危机",
     "没有大规模基础设施投资，特别是交通投资，就不可能产生让美国跻身世界工业强国的生产率的提升。"
 ]
-word_sentence_list = ws(sentences, disable_cuda=False)
+word_sentence_list = ws(sentences)
 print(word_sentence_list)
